e2c593ac0306ed295dca6e0c73beda2891cceefc
Oz_Lin <lin@ozlin.info>
2024-05-09 18:40:33 +1000

    reverted the null check due to unable to execute commands from server console

--- CountDown.cs
+++ CountDown.cs
@@ -7,6 +7,9 @@ using CounterStrikeSharp.API.Modules.Utils;
 using CounterStrikeSharp.API.Modules.Menu;
 using CounterStrikeSharp.API.Core.Attributes;
 using CounterStrikeSharp.API.Modules.Timers;
+using System;
+using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
 
 
 
@@ -72,7 +75,7 @@ public partial class CountDown : BasePlugin
                 {
                     client.PrintToCenterHtml(
                     $"<font color='gray'>----</font> <font class='fontSize-l' color='{color}'>COUNTDOWN</font><font color='gray'>----</font><br>" +
-                    $"<font color='gray'>►</font> <font class='fontSize-m' color='green'>[{Time} seconds]</font> <font color='gray'>◄</font><br>" 
+                    $"<font color='gray'>►</font> <font class='fontSize-m' color='white'>[{Time} seconds]</font> <font color='gray'>◄</font><br>" 
                     //$"<font color='gray'>----</font> <font class='fontSize-l' color='{color}'>COUNTDOWN</font><font color='gray'>----</font><br>"
                     );
                 }
@@ -81,7 +84,7 @@ public partial class CountDown : BasePlugin
                     client.PrintToCenterHtml(
                     $"<font color='gray'>----</font> <font class='fontSize-l' color='{color}'>COUNTDOWN</font><font color='gray'>----</font><br>" +
                     $"<font color='gray'>►</font> <font class='fontSize-m' color='gold'>[{Text}]</font><font color='gray'>◄</font><br>"+
-                    $"<font color='gray'>►</font> <font class='fontSize-m' color='green'>[{Time} seconds]</font> <font color='gray'>◄</font><br>" 
+                    $"<font color='gray'>►</font> <font class='fontSize-m' color='white'>[{Time} seconds]</font> <font color='gray'>◄</font><br>" 
                     //$"<font color='gray'>----</font> <font class='fontSize-l' color='{color}'>COUNTDOWN</font><font color='gray'>----</font><br>"
                     );
                 }
@@ -89,7 +92,7 @@ public partial class CountDown : BasePlugin
                 {
                     client.PrintToCenterHtml(
                     $"<font color='gray'>----</font> <font class='fontSize-l' color='{color}'>STOPWATCH</font><font color='gray'>----</font><br>" +
-                    $"<font color='gray'>►</font> <font class='fontSize-m' color='green'>[{Time} seconds]</font> <font color='gray'>◄</font><br>" 
+                    $"<font color='gray'>►</font> <font class='fontSize-m' color='white'>[{Time} seconds]</font> <font color='gray'>◄</font><br>" 
                     //$"<font color='gray'>----</font> <font class='fontSize-l' color='{color}'>STOPWATCH</font><font color='gray'>----</font><br>"
                     );
                 }
@@ -110,10 +113,11 @@ public partial class CountDown : BasePlugin
     public void CommandStartStopwatch(CCSPlayerController? player, CommandInfo info)
     {
         // add null check
-        if (player == null)
-        {
-            return;
-        }
+        // however this prevents accepting commands from server console
+        //if (player == null)
+        //{
+        //    return;
+        //}
         if (!AdminManager.PlayerHasPermissions(player, "@css/root"))
         {
             player.PrintToChat($" [{ChatColors.Lime}CountDown{ChatColors.Default}] You are not admin..");
@@ -148,14 +152,11 @@ public partial class CountDown : BasePlugin
     public void CommandStartCountDown(CCSPlayerController? player, CommandInfo info)
     {
         // add null check
-        if (player == null)
-        {
-            return;
-        }
-        if (player. == "Console")
-        {
-
-        }
+        // however this prevents accepting commands from server console
+        //if (player == null)
+        //{
+        //    return;
+        //}
         if (!AdminManager.PlayerHasPermissions(player, "@css/root"))
         {
             player.PrintToChat($" [{ChatColors.Lime}CountDown{ChatColors.Default}] You are not admin..");
@@ -198,10 +199,11 @@ public partial class CountDown : BasePlugin
     public void CommandStartTextCountDown(CCSPlayerController? player, CommandInfo info)
     {
         // add null check
-        if (player == null)
-        {
-            return;
-        }
+        // however this prevents accepting commands from server console
+        //if (player == null)
+        //{
+        //    return;
+        //}
         if (!AdminManager.PlayerHasPermissions(player, "@css/root"))
         {
             player.PrintToChat($" [{ChatColors.Lime}CountDown{ChatColors.Default}] You are not admin..");
@@ -248,4 +250,132 @@ public partial class CountDown : BasePlugin
             Time = Time - 1.0f;
         }, TimerFlags.REPEAT);
     }
+
+    /*
+    
+    [ConsoleCommand("say", "Trigger by Map ChatHUD")]
+    public void CommandStartMapTextCountDown(CCSPlayerController? player, CommandInfo info)
+    {
+        if (player == null)
+        {
+            return;
+        }
+        if (player.IsBot && player.PlayerName.Equals("Console") && player.UserId.Equals(0))
+        {
+           
+        }
+    
+
+        var TimeSec = info.ArgByIndex(1);
+        var Text_var = info.ArgByIndex(1);
+
+        if (TimeSec == null || TimeSec == "" || !IsInt(TimeSec))
+        {
+            
+            return;
+        }
+        if (Text_var == null || Text_var == "")
+        {
+            
+            return;
+        }
+        if (Countdown_enable == true || Countdown_enable_text == true || Stopwatch_enable == true)
+        {
+            player.PrintToChat($" [{ChatColors.Lime}CountDown{ChatColors.Default}] You must wait for end one countdown.");
+            return;
+        }
+        var time_convert = Convert.ToInt32(TimeSec);
+        Time = time_convert;
+        Text = Text_var;
+        Countdown_enable_text = true;
+        timer_2 = AddTimer(1.0f, () =>
+        {
+            if (Time <= 0.0)
+            {
+                Countdown_enable_text = false;
+                timer_2?.Kill();
+
+                return;
+            }
+            Random rnd = new Random();
+            int random_num = rnd.Next(5);
+            if (random_num == 0) { color = "green"; }
+            if (random_num == 1) { color = "red"; }
+            if (random_num == 2) { color = "blue"; }
+            if (random_num == 3) { color = "gold"; }
+            if (random_num == 4) { color = "orange"; }
+            Time = Time - 1.0f;
+        }, TimerFlags.REPEAT);
+
+        string filteredText = Regex.Replace(Text_var.ToLower(), @"[^a-z0-9 ]", "");
+
+        string[] words = filteredText.Split(' ');
+        int wordCount = words.Length;
+        uint triggerTimerLength = 0;
+
+        if (wordCount == 2)
+        {
+            uint.TryParse(words[1], out triggerTimerLength);
+        }
+
+        for (int i = 1; i < wordCount && triggerTimerLength == 0; i++)
+        {
+            uint currentValue = 0;
+            uint.TryParse(words[i], out currentValue);
+
+            if (i + 1 < wordCount)
+            {
+                string nextWord = words[i + 1];
+
+                if (nextWord.Length > 2 && currentValue > 0)
+                {
+                    if (nextWord.StartsWith("sec"))
+                    {
+                        triggerTimerLength = currentValue;
+                    }
+                    else if (nextWord.StartsWith("min"))
+                    {
+                        triggerTimerLength = currentValue * 60;
+                    }
+                }
+            }
+
+            if (currentValue == 0)
+            {
+                string currentWord = words[i];
+                int currentScanLength = Math.Min(currentWord.Length, 4);
+
+                for (int j = 0; j < currentScanLength; j++)
+                {
+                    if (char.IsDigit(currentWord[j]))
+                        continue;
+
+                    if (currentWord[j] == 's')
+                    {
+                        currentWord = currentWord.Substring(0, j);
+                        uint.TryParse(currentWord, out triggerTimerLength);
+                    }
+                    break;
+                }
+            }
+        }
+        
+        float currentRoundClock = CCSGameRules. - (gpGlobals.curtime - CCSGameRules.m_fRoundStartTime.Value);
+
+        if (triggerTimerLength > 4 && currentRoundClock > triggerTimerLength)
+        {
+            int triggerTime = (int)(currentRoundClock - triggerTimerLength + 0.5f);
+            int mins = triggerTime / 60;
+            int secs = triggerTime % 60;
+
+  
+            ClientPrintAll(HUD_PRINTCENTER, buf);
+        }
+        else
+        {
+
+            UTIL_SayTextFilter(filter, buf, player, messageType);
+        }
+    }
+    */
 }
--- README.md
+++ README.md
@@ -1,14 +1,3 @@
-> If you wanna you can support me on this link - **https://www.paypal.com/paypalme/deadswim**
-https://ctrlv.tv/yonT
-
-
-##### Lists of my plugins
-> [VIP](https://github.com/DeadSwimek/cs2-vip), [VIP Premium](https://github.com/DeadSwimek/cs2-vip-premium), [SpecialRounds](https://github.com/DeadSwimek/cs2-specialrounds), [Countdown](https://github.com/DeadSwimek/cs2-countdown), [CTBans](https://github.com/DeadSwimek/cs2-ctban), [HideAdmin](https://github.com/DeadSwimek/cs2-hideadmin)
-
-
-                
-
-https://ctrlv.tv/video/2023/12/02/12/zaug.webm
 
 ### Features
 
